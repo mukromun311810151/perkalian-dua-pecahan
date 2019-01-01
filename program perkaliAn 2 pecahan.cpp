db6a7a5acef28a2a3737901152e842a524ddf16c
mukromun 311810151 <45851430+mukromun311810151@users.noreply.github.com>
2019-01-01 21:37:54 +0700

    Add files via upload

---
+++ program perkaliAn 2 pecahan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+
+{
+	float p1, p2, hasil;
+	printf ("====================\n");
+	printf ("PERKALIAN 2 PECAHAN\n");
+	printf ("====================\n");
+	p1=3.0/4.0;
+	p2=2.0/3.0;
+	hasil=p1/p2;
+	printf ("%.2f / %.2f = %.2f", p1,p2,hasil);
+	getch ();
+}
